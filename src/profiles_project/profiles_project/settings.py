987eda569324527723eeb9db32ead9419091a936
teddy133 <parkerpeter634@yahoo.com>
2018-03-16 09:29:37 -0400

    Added Project and app and added Requirments22

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profiles_api'
+    'profiles_api',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
