7b965a6341b37fc5fa77c31bc18bb2b218c10aa5
teddy133 <parkerpeter634@yahoo.com>
2018-03-19 10:21:51 -0400

    added post put patch and the delete funcs

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,11 +4,25 @@ from rest_framework.views import APIView
 # Create your views here.
 
 from rest_framework.response import Response
-
+from . import serializers
+from rest_framework import status
 
 
 class HelloApiView(APIView):
     '''test an Api'''
+
+    serializer_class = serializers.HelloSerializer
+
+    def post(self, request):
+        '''create a hello messgae with the name returned'''
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message' : message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
     def get(self, request, format=None):
         '''returns a list of ApiView Features'''
@@ -18,3 +32,14 @@ class HelloApiView(APIView):
 
         ]
         return Response({'message' : 'hello', 'ApiView': an_apiview})
+
+
+    def put(self, request,pk=None):
+        'handles updating an object'
+        return Response({'method': 'put'})
+    def patch(self, request, pk=None):
+        'patch request only updates fileds provided in the request'
+        return Response({'method': 'patch'})
+    def delete(self, request, pk=None):
+        'delete an object'
+        return Response({'method': 'delete'})
